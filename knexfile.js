bbf8c606a050ea8d17ade6ace312437fff678c97
babotavo419 <one.faith419@gmail.com>
2023-08-09 13:53:29 -0400

    ok

--- knexfile.js
+++ knexfile.js
@@ -22,38 +22,5 @@ module.exports = {
         conn.run('PRAGMA foreign_keys = ON', done);  // enforce foreign key constraints with SQLite, since it's off by default
       },
     },
-},
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
-};
+}
